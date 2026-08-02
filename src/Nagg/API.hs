600e2e8f971b75d68d8f49a8b2eebf4c732bbb6b
mnuhurr <mnuhurr@pm.me>
2026-08-02 23:34:38 +0300

    category selector

--- src/Nagg/API.hs
+++ src/Nagg/API.hs
@@ -11,7 +11,7 @@ import Data.Maybe (fromMaybe)
 import Data.Proxy (Proxy(..))
 import Data.Text (Text)
 import Data.Time.Clock (UTCTime)
-import Database.SQLite.Simple (Connection, query, Only(..), FromRow(..), field)
+import Database.SQLite.Simple (Connection, query, query_, fromOnly, Only(..), FromRow(..), field)
 import Network.Wai.Handler.Warp (run)
 import Servant 
   ( type (:>), type (:<|>)(..), Get, JSON, QueryParam, Server, Handler, serve, Raw)
@@ -32,20 +32,25 @@ instance ToJSON ItemView
 
 type ItemAPI = "items" :> QueryParam "before" UTCTime
                         :> QueryParam "limit" Int
+                        :> QueryParam "category" Text
                         :> Get '[JSON] [ItemView]
 
-type API = "api" :> ItemAPI :<|> Raw
+type CategoriesAPI = "categories" :> Get '[JSON] [Text]
+
+type API = "api" :> (ItemAPI :<|> CategoriesAPI) :<|> Raw
 
 
 server :: Connection -> Server API
-server conn = getItems conn :<|> serveDirectoryFileServer "static"
+server conn = (getItems conn :<|> getCategories conn) :<|> serveDirectoryFileServer "static"
 
 runServer :: Connection -> Int -> IO ()
 runServer conn port = run port (serve (Proxy :: Proxy API) (server conn))
 
-getItems :: Connection -> Maybe UTCTime -> Maybe Int -> Handler [ItemView]
-getItems conn maybeBfr mLimit = liftIO $ do
-  let limit = fromMaybe 30 mLimit
+
+-- | items endpoint
+getItems :: Connection -> Maybe UTCTime -> Maybe Int -> Maybe Text -> Handler [ItemView]
+getItems conn maybeBfr maybeLimit maybeCat = liftIO $ do
+  let limit = fromMaybe 30 maybeLimit
   case maybeBfr of
     Nothing ->
       query conn
@@ -53,9 +58,10 @@ getItems conn maybeBfr mLimit = liftIO $ do
         \FROM item \
         \JOIN source ON item.source_id = source.id \
         \JOIN category ON source.category_id = category.id \
+        \WHERE (? IS NULL OR category.name = ?) \
         \GROUP BY item.guid \
         \ORDER BY item.published_at DESC LIMIT ?"
-        (Only limit)
+        (maybeCat, maybeCat, limit)
     Just before ->
       query conn
         "SELECT item.title, item.link, item.description, item.published_at, source.channel_title, category.name \
@@ -63,9 +69,18 @@ getItems conn maybeBfr mLimit = liftIO $ do
         \JOIN source ON item.source_id = source.id \
         \JOIN category ON source.category_id = category.id \
         \WHERE item.published_at < ? \
+        \  AND (? IS NULL OR category.name = ?) \
         \GROUP BY item.guid \
         \ORDER BY item.published_at DESC LIMIT ?"
-        (before, limit)
+        (before, maybeCat, maybeCat, limit)
+
+
+-- | categories endpoint: just return the list of available categories
+getCategories :: Connection -> Handler [Text]
+getCategories conn = liftIO $ do
+  rows <- query_ conn "SELECT name FROM category ORDER BY name" :: IO [Only Text]
+  pure $ map fromOnly rows
 
 instance FromRow ItemView where
   fromRow = ItemView <$> field <*> field <*> field <*> field <*> field <*> (T.toTitle <$> field)
+
